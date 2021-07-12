66e014086b1e97a2e62361a3f506cb976e1a7525
billsyli <billsyli@tencent.com>
2021-07-12 18:02:38 +0800

    refactor: test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App112"/>
+  <HelloWorld msg="Welcome to Your Vue.js App1123"/>
 </template>
 
 <script>
